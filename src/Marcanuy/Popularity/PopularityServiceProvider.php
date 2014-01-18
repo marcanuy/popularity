919bdb5b6f3ad742a543a678726f5651c069fe68
Marcelo Canina <me@marcanuy.com>
2014-01-18 19:25:18 -0200

    Initial commit

---
+++ src/Marcanuy/Popularity/PopularityServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Marcanuy\Popularity;
+
+use Illuminate\Support\ServiceProvider;
+
+class PopularityServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('marcanuy/popularity');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
